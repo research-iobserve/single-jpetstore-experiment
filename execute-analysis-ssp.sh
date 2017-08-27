7e6480aa9b2a1e26c2d46f6c0687eb874be1c4a1
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-08-27 23:01:38 +0200

    added special analysis for ssp paper

---
+++ execute-analysis-ssp.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# configuration
+
+BASE=$(cd "$(dirname "$0")"; pwd)/
+
+if [ -f $BASE/config ] ; then
+	. $BASE/config
+else
+	echo "Missing configuration"
+	exit 1
+fi
+
+if [ "$1" == "xmeans" ] ; then
+	echo "Running xmeans"
+	MODE="$1"
+elif [ "$1" == "em" ] ; then
+	echo "Runnong EM"
+	MODE="$1"
+else
+	echo "Unknown mode $1"
+	exit 1
+fi
+
+if [ ! -x $ANALYSIS_CLI ] ; then
+	echo "Missing analysis cli"
+	exit 1
+fi
+if [ ! -d $DATA ] ; then
+	echo "Data directory missing"
+	exit 1
+fi
+if [ ! -d $PCM ] ; then
+	echo "PCM directory missing"
+	exit 1
+fi
+
+# run analysis
+echo "------------------------"
+echo "Run analysis"
+echo "------------------------"
+
+$ANALYSIS_CLI -i "$DATA" -p "$PCM" -t 1 -v 4 -m $MODE -o "${BASE}/graph-$MODE-"
+
+# end
