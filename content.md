0a059a60c21314fc1ade0f21d65b2ef94b73c082
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-06-12 16:08:20 +0200

    added missing files

---
+++ content.md
@@ -0,0 +1,25 @@
+# Contents of this directory
+
+For a better understanding of the results, please refer to the
+analysis-result.txt file.
+
+- aggregates
+  contains all summaries of all comparisons of each ideal behavor model
+  with all cluster models for EM and X-means. The score is computed
+  based on this data.
+- comparisons
+  Contains the detail analysis output for each comparison of an ideal
+  behavior model and a clustered behavior model.
+- data
+  The used Kieker logs.
+- ideal-behavior-models
+  contains all ideal behavior models.
+- pcm
+  contains a PCM model (which was not used in our experiment).
+- results
+  contains the analysis results for EM and X-means, which are then
+  compared with the ideal-behavior-models by the compare-behavior.sh 
+- analysis-report.txt
+
+
+
---
+++ execute-analysis-and-visualization.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# configuration
+
+echo "Script does not work, as command line API of analysis changed. Please fix issue."
+exit 1
+
+BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+
+if [ -f $BASE_DIR/config ] ; then
+	. $BASE_DIR/config
+else
+	echo "Missing configuration"
+	exit 1
+fi
+
+if [ ! -f $UBM_VISUALIZATION ] ; then
+	echo "Missing user behavior visualization"
+	exit 1
+fi
+if [ ! -x $ANALYSIS ] ; then
+	echo "Missing analysis cli"
+	exit 1
+fi
+if [ ! -d $DATA_DIR ] ; then
+	echo "Data directory missing"
+	exit 1
+fi
+if [ ! -d $PCM_DIR ] ; then
+	echo "PCM directory missing"
+	exit 1
+fi
+
+
+## startup visualization
+echo "------------------------"
+echo "Build and start UBM service"
+echo "------------------------"
+docker-compose -f $UBM_VISUALIZATION build
+docker-compose -f $UBM_VISUALIZATION up >& $BASE/docker-compose.log &
+
+# deterime frontend IP address
+CID_FRONTEND=`docker ps | grep code_frontend | awk '{ print $1 }'`
+HOST_FRONTEND=`docker inspect $CID_FRONTEND | grep IPAddress | tail -1 | sed 's/.*:\ "\(.*\)",/\1/g'`
+
+# deterime logic IP address
+CID_LOGIC=`docker ps | grep code_logic | awk '{ print $1 }'`
+HOST_LOGIC=`docker inspect $CID_LOGIC | grep IPAddress | tail -1 | sed 's/.*:\ "\(.*\)",/\1/g'`
+
+URL_FRONTEND="http://$HOST_FRONTEND:3000/"
+URL_LOGIC="http://$HOST_LOGIC:8080/ubm-backend/v1/"
+
+## wait until service is available
+echo "------------------------"
+echo "Wait for service"
+echo "------------------------"
+while ! curl $URL_FRONTEND ; do
+        echo "wait frontend"
+        sleep 10
+	CID_FRONTEND=`docker ps | grep code_frontend | awk '{ print $1 }'`
+	HOST_FRONTEND=`docker inspect $CID_FRONTEND | grep IPAddress | tail -1 | sed 's/.*:\ "\(.*\)",/\1/g'`
+	URL_FRONTEND="http://$HOST_FRONTEND:3000/"
+done
+while ! curl $URL_LOGIC ; do
+        echo "wait for logic $CID_LOGIC"
+        sleep 10
+	CID_LOGIC=`docker ps | grep code_logic | awk '{ print $1 }'`
+	HOST_LOGIC=`docker inspect $CID_LOGIC | grep IPAddress | tail -1 | sed 's/.*:\ "\(.*\)",/\1/g'`
+	URL_LOGIC="http://$HOST_LOGIC:8080/ubm-backend/v1"
+done
+
+
+echo "Both web services are up. Press return to contiune"
+
+# run analysis
+echo "------------------------"
+echo "Run analysis"
+echo "------------------------"
+
+$ANALYSIS -i "$DATA" -p "$PCM" -t 1 -v 4 -u "$URL_LOGIC"
+
+echo "Analysis complete. Press return to contiune"
+
+# stop setup
+echo "------------------------"
+echo "Terminate"
+echo "------------------------"
+
+docker-compose -f $UBM_VISUALIZATION down
+
+# end
